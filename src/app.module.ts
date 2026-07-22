cbe7ea94fa9b10716bbb55726368de6a4590ca1a
supasentai <dinhluong701@gmail.com>
2026-07-22 16:02:46 +0700

    feat: add global Prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './common/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
           .valid('development', 'test', 'production')
           .default('development'),
-        PORT: Joi.number().default(3000),
+
+        PORT: Joi.number().port().default(3000),
+
         DATABASE_URL: Joi.string().required(),
       }),
+      validationOptions: {
+        abortEarly: false,
+      },
     }),
+
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
